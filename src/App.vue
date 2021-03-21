e07a01a797be9e7fddefa0e0088f71f3be5152d8
kareaachen <kareza@qq.com>
2021-03-22 00:38:54 +0800

    功能实现-页面转为PDF

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,30 @@
     <p>说明：选择csv文件上传即可将其转为二维数组（见控制台）</p>
     <a href="http://qiniu.kareza.cn/normal.csv">测试数据</a>
     <input type="file" accept=".csv" @change="onTwo" />
+
+    <h2>3⃣️.将页面转成PDF</h2>
+    <div class="row" id="pdfDom">
+      <h3>{{ fileName }}</h3>
+      <h4>这里是PDF的内容</h4>
+      <h4>这里是PDF的内容</h4>
+      <h4>这里是PDF的内容</h4>
+    </div>
+    <button @click="onThree">下载</button>
   </div>
 </template>
 
 <script>
 import SparkMD5 from "spark-md5";
 import Papa from "papaparse";
+import html2Canvas from "html2canvas";
+import JsPDF from "jspdf";
 
 export default {
   name: "app",
   data() {
     return {
       md5: "****",
+      fileName: "PDF文件名",
     };
   },
   methods: {
@@ -41,6 +53,36 @@ export default {
         complete: function (results) {
           console.log(results);
         },
+      });
+    },
+    onThree: function (event) {
+      var fileName = this.fileName;
+      html2Canvas(document.querySelector("#pdfDom"), {
+        allowTaint: true,
+        useCORS: true,
+      }).then(function (canvas) {
+        let contentWidth = canvas.width;
+        let contentHeight = canvas.height;
+        let pageHeight = (contentWidth / 592.28) * 841.89;
+        let leftHeight = contentHeight;
+        let position = 0;
+        let imgWidth = 595.28;
+        let imgHeight = (592.28 / contentWidth) * contentHeight;
+        let pageData = canvas.toDataURL("image/jpeg", 1.0);
+        let PDF = new JsPDF("", "pt", "a4");
+        if (leftHeight < pageHeight) {
+          PDF.addImage(pageData, "JPEG", 0, 0, imgWidth, imgHeight);
+        } else {
+          while (leftHeight > 0) {
+            PDF.addImage(pageData, "JPEG", 0, position, imgWidth, imgHeight);
+            leftHeight -= pageHeight;
+            position -= 841.89;
+            if (leftHeight > 0) {
+              PDF.addPage();
+            }
+          }
+        }
+        PDF.save(fileName + ".pdf");
       });
     },
   },
